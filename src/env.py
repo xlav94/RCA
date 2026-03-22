18291d8d9d140ac33f45fd3dcab95275bd866fe6
xlav94 <cedric.guevremont@gmail.com>
2026-03-22 01:57:16 -0400

    correct reward

--- src/env.py
+++ src/env.py
@@ -59,8 +59,8 @@ class CustomEnv(gym.Env):
 
     def step(self, action : np.ndarray):
         portfolio_weights = self._get_weights_from_action_mpt(action)
-        portfolio_return, transaction_penality, downside_penalty = self._calculate_reward(portfolio_weights)
-        reward = portfolio_return - transaction_penality - downside_penalty
+        portfolio_return, transaction_penality, downside_penalty, log_returns = self._calculate_reward(portfolio_weights)
+        reward = log_returns - transaction_penality - downside_penalty
         self.current_step += 1
         self.weights = portfolio_weights
 
@@ -72,7 +72,8 @@ class CustomEnv(gym.Env):
             "reward": reward,
             "portfolio_return": portfolio_return,
             "transaction_penality": transaction_penality,
-            "downside_penalty": downside_penalty
+            "downside_penalty": downside_penalty,
+            "log_returns": log_returns,
         }
         return observation, reward, terminated, truncated, info
 
@@ -132,9 +133,10 @@ class CustomEnv(gym.Env):
         # On calcule le rendement quotidien du portefeuille en utilisant les poids et les rendements des actifs
         current_prices = self.df.iloc[self.current_step].values
         previous_prices = self.df.iloc[self.current_step - 1].values
-        asset_returns = np.log((current_prices - previous_prices) / previous_prices)
+        asset_returns = (current_prices - previous_prices) / previous_prices
+        log_returns = np.log(current_prices / previous_prices)
         portfolio_return = np.dot(portfolio_weights, asset_returns)
-        downside_penalty = 0.5 * max(0, portfolio_return)**2
+        downside_penalty = 0.5 * min(0, portfolio_return)**2
 
         # Penalite si changement de poids important (pour encourager la stabilité du portefeuille)
         if self.current_step == self.window_size:
@@ -142,7 +144,7 @@ class CustomEnv(gym.Env):
         else:
             weight_change = np.sum(np.abs(portfolio_weights - self.weights)) #L1 norm
             transaction_penality = weight_change * penality_factor
-        return portfolio_return, transaction_penality, downside_penalty
+        return portfolio_return, transaction_penality, downside_penalty, log_returns
 
     def render(self):
         return

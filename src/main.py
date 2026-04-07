cef13a36378db1253aedd942cbd40f71a25a9926
xlav94 <cedric.guevremont@gmail.com>
2026-04-07 13:18:01 -0400

    ok

--- src/main.py
+++ src/main.py
@@ -103,7 +103,7 @@ def train(algo, train_seed=None):
     if checkpoint:
         checkpoint_dir = f"models/{algo}_agent_checkpoints_seed_{train_seed}_{datetime.now().strftime('%Y-%m-%d-%H-%M')}/"
         checkpoint_callback = CheckpointCallback(
-            save_freq=max(1, 5_000_000 // num_cpu),
+            save_freq=max(1, 10_000_000 // num_cpu),
             save_path=checkpoint_dir,
             name_prefix=f"{algo}_agent",
             save_replay_buffer=True,

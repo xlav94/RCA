47d4c65ff38f8b263fcc484ea132f50fa1e3f9d1
xlav94 <cedric.guevremont@gmail.com>
2026-05-01 10:12:05 -0400

    change risk adversion

--- src/main.py
+++ src/main.py
@@ -262,7 +262,7 @@ if __name__ == "__main__":
         seed_everything(train_seed)
         train(algo_type, train_seed=train_seed)
     else:
-        env = 'models/test2/PPO_seed_42_env.pkl'
-        model = 'models/test2/ppo_agent_10000000_seed_42_2026-04-30-19-33.zip'
+        env = 'models/PPO_seed_42_env.pkl'
+        model = 'models/ppo_agent_10000000_seed_42_2026-05-01-03-57.zip'
         seed_everything(test_seed)
         test(algo_type, model, env, test_seed)
